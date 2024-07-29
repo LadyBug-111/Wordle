772a0378593b1bd9e74514bd7a255d36ff3afdb4
LadyBug-111 <maia.beridze.official@gmail.com>
2024-07-30 00:51:22 +0400

    Update index.js

--- wordle/scr/index.js
+++ wordle/scr/index.js
@@ -1,9 +1,4 @@
-const dictionary = [
-    "apple", "beach", "brave", "bread", "chair", "clown", "couch", "dance", "eagle", "flame",
-    "ghost", "grape", "happy", "house", "juice", "laugh", "light", "lunch", "music", "night",
-    "paint", "piano", "plant", "river", "scarf", "smile", "space", "storm", "table", "tiger",
-    "train", "truth", "voice", "water", "wheat", "whale", "zebra"
-];
+import {dictionary } from './words.js';
 
 const state = {
     secret: dictionary[Math.floor(Math.random() * dictionary.length)],
