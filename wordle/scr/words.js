63c12fb224cfd3079aebe868c219e0a118bb0265
LadyBug-111 <maia.beridze.official@gmail.com>
2024-07-30 00:50:38 +0400

    words.js

---
+++ wordle/scr/words.js
@@ -0,0 +1,7 @@
+export const dictionary = [
+    "apple", "beach", "brave", "bread", "chair", "clown", "couch", "dance",
+    "eagle", "flame", "ghost", "grape", "happy", "house", "juice", "laugh",
+    "light", "lunch", "music", "night", "paint", "piano", "plant", "river",
+    "scarf", "smile", "space", "storm", "table", "tiger", "train", "truth",
+    "voice", "water", "wheat", "whale", "wheat", "zebra"
+];
